108a84434ea37b14f84e5db9e22fe42e68c3de4c
DarthReca <darthreca@gmail.com>
2021-05-03 10:14:25 +0200

    Data loading

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import numpy as np
+import data_loading as dl
+
 """Used to display results."""
 def main() -> None:
     """Only main."""
-    pass
+    test_data = dl.load_test_data()
     # TODO: use main
 
 if __name__ == '__main__':
