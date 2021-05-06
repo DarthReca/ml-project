42e726310a69fd2c7c0ddb08e4e69bf9681c4c03
Daniele R.C <darthreca@gmail.com>
2021-05-06 23:00:54 +0200

    Cleaned Project

--- probability-density-function/__init__.py
+++
@@ -1,2 +0,0 @@
-from .lda import between_class_covariance, lda, within_class_covariance
-from .pca import pca
--- probability-density-function/gaussian_classifier/__init__.py
+++
@@ -1,5 +0,0 @@
-from .gaussian_classifier import (apply_model, class_posterior_prob,
-                                  gaussian_estimate_stats,
-                                  log_class_posterior_prob,
-                                  naive_gaussian_estimate_stats,
-                                  tied_cov_estimate_stats)
