731807324da92424dc0a4d0998be2365381ed700
Daniele R.C <darthreca@gmail.com>
2021-05-13 22:47:12 +0200

    Added Bayes Risk functions

--- data_result_analysis/__init__.py
+++ data_result_analysis/__init__.py
@@ -1,2 +1,3 @@
 from .confusion_matrix import confusion_matrix
+from .detection_cost import dcf
 from .roc_det_curve import roc_det_curves, thresholds_error_rates
--- data_result_analysis/detection_cost.py
+++ data_result_analysis/detection_cost.py
@@ -5,16 +5,19 @@ Created on Mon May 10 23:53:59 2021
 @author: darth
 """
 
-import numpy as np
 from typing import Tuple
+
+import numpy as np
+
 from .confusion_matrix import confusion_matrix
 
-def detection_cost_function(
-        conf_m: np.ndarray,
-        prior_prob_true: float,
-        cost_fn: float,
-        cost_fp: float,
-        normalize: bool = True
+
+def dcf(
+    conf_m: np.ndarray,
+    prior_prob_true: float,
+    cost_fn: float,
+    cost_fp: float,
+    normalize: bool = True,
 ) -> float:
     """
     Compute bayes risk for given confusion matrix.
@@ -39,33 +42,49 @@ def detection_cost_function(
     fnr = conf_m[0, 1] / conf_m[:, 1].sum()
     fpr = conf_m[1, 0] / conf_m[:, 0].sum()
 
-    br = ( prior_prob_true * cost_fn * fnr +
-        (1 - prior_prob_true) * cost_fp * fpr )
+    risk = prior_prob_true * cost_fn * fnr + (1 - prior_prob_true) * cost_fp * fpr
 
-    optimal_cost = np.minimum(
-        cost_fn * prior_prob_true,
-        cost_fp * (1 - prior_prob_true)
-    )
+    optimal_cost = min(cost_fn * prior_prob_true, cost_fp * (1 - prior_prob_true))
 
     if normalize:
-        br /= optimal_cost
+        risk /= optimal_cost
 
-    return br
+    return risk
 
-def min_detection_cost_function(
-        llr_labels: Tuple[np.ndarray, np.ndarray],
-        prior_prob_true: float,
-        cost_fn: float,
-        cost_fp: float) -> float:
-    """m."""
-    llr, labels = llr_labels
-    threshold = np.arange(-100, 100, 1)
-    
-    min_br = []
-    for t in threshold:
-        selected_class = (llr > t).astype(int)
-        cm = confusion_matrix(labels, selected_class)
-        br = detection_cost_function(cm, prior_prob_true, cost_fn, cost_fp)
-        min_br.append(br)
 
-    return min(min_br)
+def min_norm_dcf(
+    scores: np.ndarray,
+    labels: np.ndarray,
+    prior_prob_true: float,
+    cost_fn: float,
+    cost_fp: float,
+) -> float:
+    """
+    Compute minimum normalized bayes risk.
+
+    Parameters
+    ----------
+    scores : np.ndarray
+        Computed scores (likelihood ratio) for samples.
+    labels : np.ndarray
+        Labels associated with samples.
+    prior_prob_true : float
+        Prior probability of true class.
+    cost_fn : float
+        Cost of false negative.
+    cost_fp : float
+        Cost of false positive.
+
+    Returns
+    -------
+    min_dcf: float
+
+    """
+    risks = []
+
+    for t in scores:
+        pred = (scores > t).astype(int)
+        cm = confusion_matrix(labels, pred)
+        risks.append(dcf(cm, prior_prob_true, cost_fn, cost_fp))
+
+    return min(risks)
